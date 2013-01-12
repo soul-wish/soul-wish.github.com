7e52f969cba92cd3e52b6483264262d9e18a076d
Sergey Lysenko <soulwish.ls@gmail.com>
2013-01-12 16:49:09 +0200

    Creating Portfolio and Contacts sections
    enabling Google Analytics

--- js/script.js
+++ js/script.js
@@ -3,4 +3,13 @@
         date = new Date();
     year = date.getFullYear();
     $('.copy').append(year);
+    $('ul.projects').roundabout();
+    $('a.info').on('click', function (e) {
+    	e.preventDefault();
+    	$(this).parent().find('.testimonial').show(300);
+    });
+    $('a.close').on('click', function (e) {
+    	e.preventDefault();
+    	$(this).parent().hide(300);
+    });
 })();
