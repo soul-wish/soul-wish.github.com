c4c1acd80a5517c41b2c00837a940a8a53fb21f3
Sergey Lysenko <soulwish.ls@gmail.com>
2013-03-28 20:47:55 +0200

    Applied new functionality and design.

--- twitterbday/js/script.js
+++ twitterbday/js/script.js
@@ -20,6 +20,34 @@ function getBday (date) {
     return bday;
 }
 
+function getDays (date) {
+    var days, digit;
+    var curDate = new Date();
+    var usrDate = new Date(date);
+    var delta = curDate - usrDate;
+    days = delta / 1000 / 60 / 60 / 24;
+    digit = 1;
+    if ((digit == 0) || (digit == 5) || (digit == 6) || (digit == 7) || (digit == 8) || (digit == 9)) {
+        days += ' дней';
+    } else if ((digit == 2) || (digit == 3) || (digit == 4)) {
+        days += ' дня';
+    } else {
+        days += ' день';
+    }
+    return days;
+}
+
+function tweetMe (results) {
+    var button = 'http://twitter.com/share?url=http%3A%2F%2Fsoulwish.pp.ua%2Ftwitterbday&text=';
+    button += getBday(results.created_at);
+    button += ' мой Твиттер День Рождения! @';
+    button += results.screen_name + ' с Твиттером уже ';
+    button += getDays(results.created_at);
+    button += '&via=@soul_wish';
+    button = button.replace(/ /gi, '%20');
+    return button;
+}
+
 function loadBirthdays () {
     var url = 'http://search.twitter.com/search.json?callback=?&rpp=15&q=to%3Acsscreatures';
     var count = 1;
@@ -33,6 +61,7 @@ function loadBirthdays () {
                 html += '<div class="icon"><img src="'+image+'" alt="'+results.name+'" /></div>';
                 html += '<a href="http://twitter.com/'+results.screen_name+'" class="name" target="_blank">'+results.name+'</a>';
                 html += '<span class="bday">' +getBday(results.created_at)+ '</span>';
+                html += '<a href="'+tweetMe(results)+'" class="tweetme">&nbsp;</a>'
                 html += '</li>';
                 tweets.append(html);
                 count++;
@@ -45,7 +74,8 @@ function loadBirthdays () {
 function loadCachedBirthdays () {
     tweets.empty();
     var user;
-    for (var i=1; i<16; i++) {
+    var storageLength = localStorage.length;
+    for (var i=1; i<=storageLength; i++) {
         user = JSON.parse(localStorage[i]);
         if (typeof user === 'object') {
             var html = '<li>';
@@ -73,8 +103,9 @@ function loadCachedBirthdays () {
         console.log('cached version');
         loadCachedBirthdays();
     };
-    $('ul.tweets li').hover(
-        function () {
-            $(this).addClass('active');
-        });
+    $('ul.tweets li').on('hover', function () {
+        $(this).addClass('active');
+    }, function () {
+        $(this).removeClass('active');
+    });
 })();
