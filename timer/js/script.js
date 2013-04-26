00c347c8c91dfacaeed2a8d113495ae1b504308b
Sergey Lysenko <soulwish.ls@gmail.com>
2013-04-26 22:15:13 +0300

    Testing canvas

--- timer/js/script.js
+++ timer/js/script.js
@@ -7,28 +7,29 @@
 		if (seconds == 60) {
 			minutes++;
 			seconds = 0;
+			var canvas = document.createElement('canvas');
+		    canvas.width = 16;canvas.height = 16;
+		    var ctx = canvas.getContext('2d');
+		    var img = new Image();
+		    img.src = 'favicon.ico';
+		    img.onload = function() {
+		        ctx.drawImage(img, 0, 0);
+		        ctx.fillStyle = "#F00";
+		        ctx.fillRect(10, 7, 6, 8);
+		        ctx.fillStyle = '#FFFFFF';
+		        ctx.font = 'bold 10px sans-serif';
+		        ctx.fillText(minutes, 10, 14);
+
+		        var link = document.createElement('link');
+		        link.type = 'image/x-icon';
+		        link.rel = 'shortcut icon';
+		        link.href = canvas.toDataURL("image/x-icon");
+		        document.getElementsByTagName('head')[0].appendChild(link);
+		    }
 		}
 		time = minutes + ':' + seconds;
 
-		var canvas = document.createElement('canvas');
-	    canvas.width = 16;canvas.height = 16;
-	    var ctx = canvas.getContext('2d');
-	    var img = new Image();
-	    img.src = 'favicon.ico';
-	    img.onload = function() {
-	        ctx.drawImage(img, 0, 0);
-	        ctx.fillStyle = "#F00";
-	        ctx.fillRect(10, 7, 6, 8);
-	        ctx.fillStyle = '#FFFFFF';
-	        ctx.font = 'bold 10px sans-serif';
-	        ctx.fillText(minutes, 10, 14);
-
-	        var link = document.createElement('link');
-	        link.type = 'image/x-icon';
-	        link.rel = 'shortcut icon';
-	        link.href = canvas.toDataURL("image/x-icon");
-	        document.getElementsByTagName('head')[0].appendChild(link);
-	    }
+		
 
 		$('title').text(time);
 	}
