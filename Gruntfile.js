50732a91dce5ece36c1b5a9de047121269e9b114
soulwish <soulwish.ls@gmail.com>
2013-12-21 22:57:24 +0200

    Added translations. Still russian is default language.. Enabled Grunt. Improved CSS styles.

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        uglify: {
+            my_target: {
+                files: {
+                    'js/script.min.js': ['js/script.js']
+                }
+            }
+        },
+
+        cssmin: {
+            combine: {
+                files: {
+                    'css/style.min.css': ['css/style.css']
+                }
+            }
+        },
+
+        watch: {
+            css: {
+                files: ['css/style.css'],
+                tasks: ['cssmin'],
+                options: {
+                    livereload: true
+                }
+            },
+            js: {
+                files: ['js/script.js'],
+                tasks: ['uglify'],
+                options: {
+                    livereload: true
+                }
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['uglify', 'cssmin', 'watch']);
+
+};
--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 (function () {
     var year,
-        date = new Date();
+        date = new Date(),
+        oldTranslate,
+        newTranslate,
+        enRu = $('.en-ru'),
+        translate = $('.translate');
     year = date.getFullYear();
     $('.copy').append(year);
     $('ul.projects').roundabout({
@@ -15,5 +19,12 @@
     $('a.close').on('click', function (e) {
     	e.preventDefault();
     	$(this).parent().hide(300);
+    });
+    enRu.on('click', function(){
+        translate.each(function() {
+            oldTranslate = $(this).text();
+            newTranslate = $(this).attr('data-translate');
+            $(this).attr('data-translate', oldTranslate).text(newTranslate);
+        });
     });
 })();
