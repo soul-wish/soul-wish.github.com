d802a9ee03a4093aef86ee44c09dea60c3224cc6
soulwish <soulwish.ls@gmail.com>
2014-04-01 22:24:23 +0300

    Added comments

--- tweetfeed/js/script.js
+++ tweetfeed/js/script.js
@@ -5,4 +5,8 @@ $(function() {
     server = new Firebase("https://boiling-fire-1984.firebaseio.com");
     messages = server.child("messages");
     messages.push({name: "Soulwish", text: "Hello!"});
+    // check if name in localstorage exists
+    // if it doesnt - show auth form (just 'insert name' - something like this)
+    // save name in localstorage and show chat
+    // if name exists - just show chat
 });
