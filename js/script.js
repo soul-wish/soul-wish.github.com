d86ec43d8a5ec64c2629aacf4b6fc527cc44c3fd
Soulwish <soulwish.ls@gmail.com>
2014-12-14 18:06:48 +0200

    add christmas parallax background effect

--- js/script.js
+++ js/script.js
@@ -4,22 +4,61 @@
         oldTranslate,
         newTranslate,
         enRu = $('.en-ru'),
-        translate = $('.translate');
+        translate = $('.translate'),
+        pageBg1 = $('.page-bg1'),
+        pageBg2 = $('.page-bg2'),
+        pageBg3 = $('.page-bg3');
+
+    $.fn.bgParallax = function(options) {
+        var defaults = { moveFactor: 5, zIndexValue: "-1", targetContainer: 'body' };
+    
+        var options = $.extend(defaults, options);
+    
+        return this.each(function() {
+            var o = options;
+            var background = $(this);
+            
+            $(o.targetContainer).on('mousemove', function(e){
+            
+                mouseX = e.pageX;
+                mouseY = e.pageY;
+                
+                windowWidth = $(window).width();
+                windowHeight = $(window).height();
+                
+                percentX = ((mouseX/windowWidth)*o.moveFactor) - (o.moveFactor/2);
+                percentY = ((mouseY/windowHeight)*o.moveFactor) - (o.moveFactor/2);
+
+                leftString = (0-percentX-o.moveFactor)+"%";
+                rightString = (0-percentX-o.moveFactor)+"%";
+                topString = (0-percentY-o.moveFactor)+"%";
+                bottomString = (0-percentY-o.moveFactor)+"%";
+
+                background[0].style.left = leftString;
+                // background[0].style.right = rightString;
+                background[0].style.top = topString;
+                // background[0].style.bottom = bottomString;
+                if(o.zIndexValue) { 
+                    background[0].style.zIndex = o.zIndexValue;
+                }
+            });
+        });
+    };
+    
+    pageBg1.bgParallax({
+        moveFactor: 1
+    });
+
+    pageBg2.bgParallax({
+        moveFactor: 3
+    });
+
+    pageBg3.bgParallax({
+        moveFactor: 9
+    });
+
     year = date.getFullYear();
     $('.copy').append(year);
-    $('ul.projects').roundabout({
-        autoplay: 'true',
-        autoplayDuration: 8000,
-        autoplayPauseOnHover: 'true'
-    });
-    $('a.info').on('click', function (e) {
-    	e.preventDefault();
-    	$(this).parent().find('.testimonial').show(300);
-    });
-    $('a.close').on('click', function (e) {
-    	e.preventDefault();
-    	$(this).parent().hide(300);
-    });
     enRu.on('click', function(){
         translate.each(function() {
             oldTranslate = $(this).text();
