ab4f111d29b9ca152c41db2e413ef34c014752b3
Sergey Lysenko <soulwish.ls@gmail.com>
2022-04-27 15:44:03 +0300

    Update content loading logic

--- src/lib/content.js
+++ src/lib/content.js
@@ -1,41 +1,49 @@
-import { resolve, basename } from 'path';
+import { resolve } from 'path';
 import { compile } from 'mdsvex';
-import grayMatter from 'gray-matter';
-import { promises as fs } from 'fs';
 
 export async function listBlogposts() {
 	let content = [];
-	for await (const _path of getFiles('src/lib/posts')) {
-		const src = await fs.readFile(_path, 'utf8');
-		const data = grayMatter(src);
+	const modules = import.meta.globEager('/posts/*.md');
+	Object.entries(modules).forEach(([_, module]) => {
+		const { metadata } = module;
 		content.push({
-			content: data.content,
-			data: data.data,
-			slug: data.data.slug ?? basename(_path, '.md')
+			data: metadata
 		});
-	}
+	});
+
 	return content;
 }
 
 export async function getBlogpost(slug) {
-	const _path = resolve('src/lib/posts', slug + '.md');
-	const src = await fs.readFile(_path, 'utf8');
-	const data = grayMatter(src);
-	const content = (await compile(data.content, {})).code
-		// https://github.com/pngwn/MDsveX/issues/392
-		.replace(/>{@html `<code class="language-/g, '><code class="language-')
-		.replace(/<\/code>`}<\/pre>/g, '</code></pre>');
-	return { content, data: data.data, slug: data.data.slug ?? basename(_path, '.md') };
+	const modules = import.meta.globEager(`/posts/*.md`);
+	const module = modules[resolve(`/posts/${slug}.md`)];
+	const { metadata } = module;
+	console.log('module', module);
+	const { html } = module.default.render();
+	// const content = (await compile(html, {})).code;
+	// html
+	// 	.replace(/>{@html `<code class="language-/g, '><code class="language-')
+	// 	.replace(/<\/code>`}<\/pre>/g, '</code></pre>');
+	return { content: html, data: metadata };
+
+	// const _path = resolve('posts', slug + '.md');
+	// const src = await fs.readFile(_path, 'utf8');
+	// const data = grayMatter(src);
+	// // const content = (await compile(data.content, {})).code
+	// 	// https://github.com/pngwn/MDsveX/issues/392
+	// 	.replace(/>{@html `<code class="language-/g, '><code class="language-')
+	// 	.replace(/<\/code>`}<\/pre>/g, '</code></pre>');
+	// return { content, data: data.data, slug: data.data.slug ?? basename(_path, '.md') };
 }
 
-async function* getFiles(dir) {
-	const dirents = await fs.readdir(dir, { withFileTypes: true });
-	for (const dirent of dirents) {
-		const res = resolve(dir, dirent.name);
-		if (dirent.isDirectory()) {
-			yield* getFiles(res);
-		} else {
-			yield res;
-		}
-	}
-}
+// async function* getFiles(dir) {
+// 	const dirents = await fs.readdir(dir, { withFileTypes: true });
+// 	for (const dirent of dirents) {
+// 		const res = resolve(dir, dirent.name);
+// 		if (dirent.isDirectory()) {
+// 			yield* getFiles(res);
+// 		} else {
+// 			yield res;
+// 		}
+// 	}
+// }
--- src/mdsvexlayout.svelte
+++ src/mdsvexlayout.svelte
@@ -1,8 +1,7 @@
 <!-- https://tailwindcss.com/docs/typography-plugin#element-modifiers -->
 <article
-	class="container prose mx-auto mb-12 hover:prose-a:text-blue-300
-prose-ul:list-disc
-dark:prose-invert
+	class="w-full container prose mb-12 max-w-none hover:prose-a:text-blue-300
+prose-ul:list-disc dark:prose-invert
 "
 >
 	<slot />
