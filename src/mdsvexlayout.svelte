0ffe96bcf46de028b9a62753055c020f6a546323
Sergey Lysenko <soulwish.ls@gmail.com>
2022-04-27 16:03:58 +0300

    Fix mdsvex layout

--- src/mdsvexlayout.svelte
+++ src/mdsvexlayout.svelte
@@ -1,8 +1,9 @@
 <!-- https://tailwindcss.com/docs/typography-plugin#element-modifiers -->
-<article
-	class="w-full container prose mb-12 max-w-none hover:prose-a:text-blue-300
-prose-ul:list-disc dark:prose-invert
-"
->
-	<slot />
-</article>
+<div class="flex flex-col items-start justify-center w-full max-w-2xl mx-auto mb-16">
+	<article
+		class="w-full container prose mb-12 max-w-none hover:prose-a:text-blue-300
+		prose-ul:list-disc dark:prose-invert"
+	>
+		<slot />
+	</article>
+</div>
