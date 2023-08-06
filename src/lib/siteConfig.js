95a4b24ca043abedad8dd5af80bdb38f735f107f
Sergii Lysenko <soulwish.ls@gmail.com>
2023-08-05 20:52:34 -0400

    fix of image

--- src/lib/siteConfig.js
+++ src/lib/siteConfig.js
@@ -1,6 +1,6 @@
 export const SITE_URL = 'https://soulwish.info';
 export const SITE_TITLE = 'Sergii Lysenko – front-end developer';
 export const SITE_DESCRIPTION = 'Passionate front-end developer from Ukraine.';
-export const DEFAULT_OG_IMAGE = '/images/banner.png';
+export const DEFAULT_OG_IMAGE = 'https://soulwish.info/images/banner.png';
 export const MY_TWITTER_HANDLE = 'soul_wish';
 export const MY_GITHUB_HANDLE = 'soul-wish';
